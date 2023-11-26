9fa7220a4ac01e55cd0747e3d7068be846ed9222
tigeryu8900 <tigeryu8900@gmail.com>
2023-11-26 01:42:03 -0800

    don't create new element upon new injection

--- userscript-polyfill.js
+++ userscript-polyfill.js
@@ -61,6 +61,58 @@ let __polyfill_loader__ = (async () => {
 
     try {
         let insertPWALink = __userscript_location__.origin === "https://mangadex.org";
+        let leftPWALink = $(String.raw`
+            <a data-v-69037ae7="" data-v-eba09a86="" href="/pwa/" class="flex-shrink-0" title="">
+                <div data-v-eba09a86="" class="px-4 pt-2 flex flex-col flex-shrink-0">
+                    <div data-v-abcd45c8="" data-v-eba09a86="" class="list__item">
+                        <div data-v-abcd45c8="">
+                            <img data-v-4c681a64=""
+                                    data-v-abcd45c8=""
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    class="feather feather-book-open icon"
+                                    viewBox="0 0 24 24"
+                                    style="color: currentcolor;"
+                                    src="/icons/mal-sync-icon.svg">
+                        </div>
+                        <div data-v-abcd45c8="" class="mx-2 font-bold">MALSync</div>
+                    </div>
+                </div>
+            </a>
+        `);
+        let rightPWALink = $(String.raw`
+            <a data-v-a31e942f=""
+               data-v-c1dca64c=""
+               class="list__item mt-1 rounded custom-opacity relative md-btn flex items-center px-3 overflow-hidden accent text px-4 list__item mt-1"
+               style="min-height: 3rem; min-width: 100%;"
+               href="/pwa/">
+                <span data-v-a31e942f=""
+                      class="flex relative items-center justify-center font-medium select-none w-full"
+                      style="pointer-events: none; justify-content: stretch;">
+                    <div data-v-c1dca64c="" class="flex justify-between items-center w-full">
+                        <span data-v-c1dca64c="" class="mr-1 whitespace-nowrap">MALSync</span>
+                        <img data-v-4c681a64=""
+                                data-v-abcd45c8=""
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24" height="24" fill="none"
+                                stroke="currentColor"
+                                stroke-linecap="round"
+                                stroke-linejoin="round" 
+                                stroke-width="2"
+                                class="feather feather-book-open icon"
+                                viewbox="0 0 24 24"
+                                style="color: currentcolor;"
+                                src="/icons/mal-sync-icon.svg">
+                    </div>
+                </span>
+            </a>
+        `);
         const observer = new (MutationObserver || WebkitMutationObserver)(mutationList => {
             for (const mutation of mutationList) {
                 if (mutation.type === "childList") {
@@ -68,62 +120,12 @@ let __polyfill_loader__ = (async () => {
                         let home = $('#section-Home:not([__userscript_transformed__])');
                         if (home.length) {
                             home.attr("__userscript_transformed__", "");
-                            home.after(String.raw`
-                                <a data-v-69037ae7="" data-v-eba09a86="" href="/pwa/" class="flex-shrink-0" title="">
-                                    <div data-v-eba09a86="" class="px-4 pt-2 flex flex-col flex-shrink-0">
-                                        <div data-v-abcd45c8="" data-v-eba09a86="" class="list__item">
-                                            <div data-v-abcd45c8="">
-                                                <object data-v-4c681a64=""
-                                                        data-v-abcd45c8=""
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                        width="24"
-                                                        height="24"
-                                                        fill="none"
-                                                        stroke="currentColor"
-                                                        stroke-linecap="round"
-                                                        stroke-linejoin="round"
-                                                        stroke-width="2"
-                                                        class="feather feather-book-open icon"
-                                                        viewBox="0 0 24 24"
-                                                        style="color: currentcolor;"
-                                                        data="/icons/mal-sync-icon.svg"></object>
-                                            </div>
-                                            <div data-v-abcd45c8="" class="mx-2 font-bold">MALSync</div>
-                                        </div>
-                                    </div>
-                                </a>
-                            `);
+                            leftPWALink.insertAfter(home);
                         }
                         let grid = $('div:is(.drawer, .profile__container) > div > div.grid:not([__userscript_transformed__])');
                         if (grid.length) {
                             grid.attr("__userscript_transformed__", "");
-                            grid.after(String.raw`
-                                <a data-v-a31e942f=""
-                                   data-v-c1dca64c=""
-                                   class="list__item mt-1 rounded custom-opacity relative md-btn flex items-center px-3 overflow-hidden accent text px-4 list__item mt-1"
-                                   style="min-height: 3rem; min-width: 100%;"
-                                   href="/pwa/">
-                                    <span data-v-a31e942f=""
-                                          class="flex relative items-center justify-center font-medium select-none w-full"
-                                          style="pointer-events: none; justify-content: stretch;">
-                                        <div data-v-c1dca64c="" class="flex justify-between items-center w-full">
-                                            <span data-v-c1dca64c="" class="mr-1 whitespace-nowrap">MALSync</span>
-                                            <object data-v-4c681a64=""
-                                                    data-v-abcd45c8=""
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    width="24" height="24" fill="none"
-                                                    stroke="currentColor"
-                                                    stroke-linecap="round"
-                                                    stroke-linejoin="round" 
-                                                    stroke-width="2"
-                                                    class="feather feather-book-open icon"
-                                                    viewbox="0 0 24 24"
-                                                    style="color: currentcolor;"
-                                                    data="/icons/mal-sync-icon.svg"></object>
-                                        </div>
-                                    </span>
-                                </a>
-                            `);
+                            rightPWALink.insertAfter(grid);
                         }
                     }
                     for (let element of mutation.addedNodes) {
